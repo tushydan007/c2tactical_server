b8ef54f32839238fabfdc00e0f7183de63364618
Cornelius Ezeh <cornzeh@gmail.com>
2025-11-29 12:55:07 +0100

    Changed the superuser created in the entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,9 @@ if [ "$DJANGO_ROLE" != "worker" ]; then
   python manage.py shell <<EOF
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(email='admin@example.com').exists():
+if not User.objects.filter(email='admin@c2tactical.com').exists():
     User.objects.create_superuser(
-        email='admin@example.com',
+        email='admin@c2tactical.com',
         password='admin123',
         first_name='Admin',
         last_name='User'
